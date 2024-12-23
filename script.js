97329c3f906cb1b56aba4ee28bd195096bfd8618
Nelson Mensah H. <nelsonh@outlook.fr>
2024-12-23 17:19:24 +0100

    fixed close dialog button

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// Récupérer tous les éléments ayant un dialog
 const cards = document.querySelectorAll(".card");
 
-// Ajouter des événements pour chaque carte
 cards.forEach((card) => {
-  const dialog = card.querySelector(".dialog"); // Dialog associé à la carte
+  const dialog = card.querySelector(".dialog");
   const closeButton = dialog.querySelector(".close-dialog");
 
-  // Ouvrir le dialog au clic sur la carte
   card.addEventListener("click", () => {
     dialog.showModal();
   });
-
-  // Fermer le dialog au clic sur le bouton "Fermer"
-  closeButton.addEventListener("click", () => {
+  closeButton.addEventListener("click", (e) => {
+    e.stopPropagation();
     dialog.close();
   });
 });
