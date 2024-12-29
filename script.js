859be785d46d24a14a883d89f3420e1723db8558
Nelson Mensah H. <nelsonh@outlook.fr>
2024-12-29 10:39:25 +0100

    finished

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const cards = document.querySelectorAll(".card");
 
-cards.forEach((card) => {
-  const dialog = card.querySelector(".dialog");
-  const closeButton = dialog.querySelector(".close-dialog");
-
-  card.addEventListener("click", () => {
-    dialog.showModal();
-  });
-  closeButton.addEventListener("click", (e) => {
-    e.stopPropagation();
-    dialog.close();
-  });
-});
